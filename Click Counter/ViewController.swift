56f95c7f8511463621e95f417e6c9d6f07190124
Val Klump <vklump@google.com>
2015-05-06 15:00:51 -0700

    add two buttons with counter; buttons change alpha value of view's background

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count:Int = 50
+    
+    var label:UILabel!
+
+    var bgColor = UIColor.orangeColor()
+    var alpha:CGFloat!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Background color
+        changeBGColor()
+        
+        // Label
+        var label = UILabel()
+        label.frame = CGRectMake(150, 150, 60, 60)
+        label.text = "\(count)"
+        
+        self.view.addSubview(label)
+        self.label = label
+        
+        // Increment Button
+        var addButton = UIButton()
+        addButton.frame = CGRectMake(150, 250, 200, 60)
+        addButton.setTitle("Tap to Add", forState: .Normal)
+        addButton.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Left
+        addButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        addButton.setTitleColor(UIColor.redColor(), forState: .Highlighted)
+        self.view.addSubview(addButton)
+    
+        addButton.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        // Decrement Button
+        var decButton = UIButton()
+        decButton.frame = CGRectMake(150, 350, 200, 60)
+        decButton.setTitle("Tap to Subtract", forState: .Normal)
+        decButton.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Left
+        decButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        decButton.setTitleColor(UIColor.redColor(), forState: .Highlighted)
+        self.view.addSubview(decButton)
+        
+        decButton.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func incrementCount() {
+        self.count++
+        changeBGColor()
+        self.label.text = "\(self.count)"
+        
     }
-
+    
+    func decrementCount() {
+        self.count--
+        changeBGColor()
+        self.label.text = "\(self.count)"
+    }
+    
+    func changeBGColor() {
+        alpha = CGFloat(abs(count)) / 100
+        bgColor = bgColor.colorWithAlphaComponent(alpha)
+        self.view.backgroundColor = bgColor
+    }
 
 }
 
